f65504552c181f791e7508625e8072e98c770e90
Michael Stal <michael.stal@gmail.com>
2021-05-21 22:20:59 +0200

    Added comment

--- src/Sleep.cpp
+++ src/Sleep.cpp
@@ -130,7 +130,7 @@ void Sleep::after_sleep() {
     scb_hw->scr             = _scb_orig;
     clocks_hw->sleep_en0    = _en0_orig;
     clocks_hw->sleep_en1    = _en1_orig;
-    _loop();
+    _loop(); // calling user-defined loop() function
 }
 
 
