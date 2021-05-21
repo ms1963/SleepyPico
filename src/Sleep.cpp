7f6cb439a2d8853203297fe7d99868d86c3d5786
Michael Stal <michael.stal@gmail.com>
2021-05-21 22:00:55 +0200

    New version
    Some design improvements in Sleep.cpp, Sleep.hpp, SleepyPico.cpp

---
+++ src/Sleep.cpp
@@ -0,0 +1,159 @@
+/*
+ Class Sleep provides basic support for operating
+ a Raspberry Pi Pico in sleep or dormant mode.
+
+ (c) 2021, by Michael Stal
+ This library is published under GPL 3.0 license. 
+*/
+
+#include "Sleep.hpp"
+#include "pico/stdlib.h"
+#include "pico/sleep.h"
+#include "hardware/clocks.h"
+#include "hardware/rosc.h"
+#include "hardware/structs/scb.h"
+#include "hardware/pll.h"
+
+
+// configure for SLEEP mode
+// loop:      lambda of function being called after each
+//            deep sleep period
+// setup:     lambda of function that is called once
+//            used for setting up the environment 
+// startTime: initial datetime for the RTC
+// endTime:   time when alarm should be fired in
+//            SLEEP mode
+void Sleep::configure(std::function<void()> setup, std::function<void()> loop, 
+                    datetime_t startTime, datetime_t endTime) {
+    _mode       = MODE::SLEEP;
+    _loop       = loop;
+    _setup      = setup;
+    _init_time  = startTime;
+    _alarm_time = endTime;
+}
+
+// configure for DORMANT mode
+// loop:      lambda of function being called after each
+//            deep sleep period
+// setup:     lambda of function that is called once
+//            used for setting up the environment needed
+// WAKEUP_PIN:Pin used for detecting GPIO status
+//            in DORMANT mode
+// edge:      interrupt on leading edge (true) or trailing edge (false)
+// active:    pin used with Active HIGH (true) or Active LOW (false)
+void Sleep::configure(std::function<void()> setup, std::function<void()> loop, uint WAKEUP_PIN, bool edge, bool active) {
+    _mode       = MODE::DORMANT;
+    _loop       = loop;
+    _setup      = setup;
+    _wakeup_pin = WAKEUP_PIN;
+    _edge       = edge;
+    _active     = active;
+}
+
+// configure for NORMAL mode
+// loop:      lambda of function being called after each
+//            deep sleep period
+// setup:     lambda of function that is called once
+//            used for setting up the environment needed
+void Sleep::configure(std::function<void()> setup, std::function<void()> loop) {
+    _mode       = MODE::NORMAL;
+    _loop       = loop;
+    _setup      = setup;   
+}
+
+// helper function to display frequencies of Pico system clocks
+void Sleep::measure_freqs(void) {
+    uint f_pll_sys   = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb   = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc      = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys   = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri  = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb   = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc   = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc   = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+
+    printf("pll_sys  = %dkHz\n", f_pll_sys);
+    printf("pll_usb  = %dkHz\n", f_pll_usb);
+    printf("rosc     = %dkHz\n", f_rosc);
+    printf("clk_sys  = %dkHz\n", f_clk_sys);
+    printf("clk_peri = %dkHz\n", f_clk_peri);
+    printf("clk_usb  = %dkHz\n", f_clk_usb);
+    printf("clk_adc  = %dkHz\n", f_clk_adc);
+    printf("clk_rtc  = %dkHz\n", f_clk_rtc);
+   
+    stdio_flush();
+}
+
+// saves clock registers
+// Initialize RTC for sleep mode
+void Sleep::before_sleep() {
+    // save current values for clocks
+    _scb_orig = scb_hw->scr;
+    _en0_orig = clocks_hw->sleep_en0;
+    _en1_orig = clocks_hw->sleep_en1;
+}
+
+static void onWakeUp() {
+    // actions for wake up event
+    tight_loop_contents();
+}
+
+// this function is responsible for sleep
+// sleep ends with high edge (DORMANT mode) 
+// or when _alarm_time is reached (SLEEP mode)
+void Sleep::start_sleep() {
+    // Crystal oscillator drives RTC during sleep
+    sleep_run_from_xosc();
+    // Reset real time clock to a value
+    // see the defines for MINUTES_TO_WAIT, SECONDS_TO_WAIT
+    if (_mode == MODE::SLEEP) { // sleep until RTC triggers alarm
+        rtc_init();
+        rtc_set_datetime(&_init_time);
+        sleep_goto_sleep_until(&_alarm_time, &onWakeUp);
+    } 
+    else 
+    if (_mode == MODE::DORMANT) { 
+        //Go to sleep until we see a leading edge (_edge = true) or 
+        // trailing edge (_edge = false)
+        // with the Pin being active high (_active = true) or 
+        // low (_active = false)
+        sleep_goto_dormant_until_pin(_wakeup_pin, _edge, _active);
+    }
+}
+
+// sleep recovery
+void Sleep::after_sleep() {
+    // re-initialize clocks
+    clocks_init();
+    // Re-enable Ring Oscillator control
+    rosc_write(&rosc_hw->ctrl, ROSC_CTRL_ENABLE_BITS);
+
+    // restore clock registers
+    scb_hw->scr             = _scb_orig;
+    clocks_hw->sleep_en0    = _en0_orig;
+    clocks_hw->sleep_en1    = _en1_orig;
+    _loop();
+}
+
+
+// Implementation of event loop
+// 1. _setup() is being executed
+// 2. the sleep functionality is executed
+//      A) begin_sleep
+//      B) start_sleep
+//      C) after_sleep
+//    Within after_sleep _loop() is called
+void Sleep::run() {
+    _setup(); // called once
+    while(true) {
+        if (_mode != MODE::NORMAL) {
+            before_sleep();
+            start_sleep(); 
+            after_sleep(); // here _loop gets called in each iteration
+        }
+        else {
+            _loop(); // NORMAL mode =>
+                     // must explicitly call _loop
+        }
+    }
+}
---
+++ src/SleepyPico.cpp
@@ -0,0 +1,221 @@
+/*
+ SleepyPico uses sleep or dormant mode to put the Raspberry Pi Pico
+ into sleep for low power consumption.
+ The system frequency is reduced additionally.
+ In the circuit a BME280 is used for measuring temperature, pressure,
+ humidity. The BME280 is accessed using forced mode to let the sensor
+ sleep between measurements.
+ Information is displayed via a SSD1306 OLED display. Other similar 
+ displays might be used instead. The OLED is also put into sleep mode
+ between sleep phases (power off).
+ 
+ You may also set the mode (SLEEP, DORMANT, NORMAL), the system frequency,
+ the sleep time using the definitions in the beginning of this file.
+ Unfortunately, the sleep and dormant modes do not operate correctly so 
+ that the Pico gets stuck from time to time. So far, I could not
+ find any hints indicating the reason for this problem.
+
+ (c) 2021, by Michael Stal
+ This example is published under GPL 3.0 license. 
+*/
+
+
+#include "Sleep.hpp"
+#include "bme280_spi.hpp"
+#include "ss_oled.hpp"
+
+
+
+
+// MODE::DORMANT: Select the GPIO-Pin on which 
+// a high edge wakes the Pico up. In
+// addition, set whether waiting for leading
+// or trailing edge, and whether the Pin is
+// active low or high.
+//
+// MODE::SLEEP: Set the time after which the Pico 
+// should wake up using the RTC - valid for  
+// Change these values according to your needs.
+// Maximum wait time is 59 mminutes and 59 seconds.
+// If you need longer sleep time, change the two
+// datetime_t structs _start_time & and _init_time below
+//
+// MODE::NORMAL
+
+
+
+#define MINUTES_TO_WAIT         0       // MODE::SLEEP only: sleeping for <MINUTES_TO_WAIT> minutes
+#define SECONDS_TO_WAIT         20      //                   and <SECONDS_TO_WAIT> seconds
+#define SYSTEM_FREQUENCY_KHZ    60000   // target frequency of Pico
+#define DISPLAY_TIME            10000   // time in milliseconds to show the measurement
+
+
+// OLED SSD1306 (I2C) and RPI Pico
+#define SDA_PIN         4
+#define SCL_PIN         5
+#define PICO_I2C        i2c0
+#define I2C_SPEED       100 * 1000
+#define OLED_WIDTH      128
+#define OLED_HEIGHT     64
+
+int oled_rc; // stores return codes of OLED driver
+
+// BUILT-IN LED
+const uint LED_PIN      = 25; // used to signal measurement
+const uint WAKEUP_PIN   = 15; // used to trigger wake-up in dormant mode MODE::DORMANT
+
+// start time to which RTC is set
+datetime_t start = {   // time to which RTC is set
+    .year  = 2021,
+    .month = 05,
+    .day   = 01,
+    .dotw  = 6, // 0 is Sunday, so 5 is Friday
+    .hour  = 00,
+    .min   = 00,
+    .sec   = 00
+ };
+
+// time when the RTC shall wake us up
+ datetime_t end = {   // time of alarm
+    .year  = 2021,
+    .month = 05,
+    .day   = 01,
+    .dotw  = 6, // 0 is Sunday, so 5 is Friday
+    .hour  = 00,
+    .min   = MINUTES_TO_WAIT,
+    .sec   = SECONDS_TO_WAIT
+ };
+
+ uint8_t ucBuffer[1024]; // buffer used for OLED
+
+/*
+* prints measurements to OLED display
+*/
+void draw_on_oled(picoSSOLED myOled, BME280::Measurement_t values) {  
+    myOled.fill(0,1);
+    myOled.power(true); // display on
+    char tem[30]; // buffer for displaying temperature on oled
+    char hum[30]; // buffer for displaying humidity    on oled
+    char prs[30]; // buffer for displaying pressure    on oled
+    char alt[30]; // buffer for displaying altitude    on oled
+
+    // create formatted strings with measurement results
+    sprintf(tem, "tem: %6.1f C",   values.temperature);
+    sprintf(hum, "hum: %6.1f %c",  values.humidity, '%');
+    sprintf(prs, "prs: %6.1f hPa", values.pressure);
+    sprintf(alt, "alt: %6.1f m",   values.altitude);
+
+    if (oled_rc != OLED_NOT_FOUND)
+    { 
+        myOled.set_contrast(127);
+        myOled.write_string(0,0,1,(char *)" Weather Today ", FONT_8x8, 0, 1);
+        myOled.write_string(0,0,3,tem, FONT_8x8, 0, 1); // write temperature
+        myOled.write_string(0,0,4,hum, FONT_8x8, 0, 1); // write humidity
+        myOled.write_string(0,0,5,prs, FONT_8x8, 0, 1); // write pressure
+        myOled.write_string(0,0,6,alt, FONT_8x8, 0, 1); // write altitude
+
+        gpio_put(LED_PIN, 0); // Turn off LED
+         
+        sleep_ms(DISPLAY_TIME);    // wait so that user can read the display
+        myOled.power(false);       // display off
+    }
+}
+
+/*
+* prints welcome screen to OLED display
+*/
+void welcome(picoSSOLED myOled) {  
+    myOled.write_string(0,0,1,(char *)" Weather Today ", FONT_8x8, 0, 1);
+    switch(Sleep::instance().get_mode()) {
+        case Sleep::MODE::SLEEP:
+            myOled.write_string(0,0,3, (char*)" SLEEP mode", FONT_8x8, 0, 1); 
+            break;
+        case Sleep::MODE::DORMANT:
+            myOled.write_string(0,0,3, (char*)" DORMANT mode", FONT_8x8, 0, 1); 
+            break;       
+        default:
+            myOled.write_string(0,0,3, (char*)" NORMAL MODE", FONT_8x8, 0, 1); 
+    }
+    sleep_ms(3000);
+    myOled.power(false);
+}
+
+// initializing the BME280
+BME280 myBME280(0, 
+            PICO_DEFAULT_SPI_RX_PIN, 
+            PICO_DEFAULT_SPI_TX_PIN, 
+            PICO_DEFAULT_SPI_SCK_PIN, 
+            PICO_DEFAULT_SPI_CSN_PIN, 
+            500 * 1000,
+            BME280::MODE::MODE_FORCED); // using BME280 in forced mode to reduce energy consumption
+
+// initializing the OLED display
+picoSSOLED myOled(OLED_128x64, 0x3c, 0, 0, PICO_I2C, SDA_PIN, SCL_PIN, I2C_SPEED);
+
+BME280::Measurement_t result;
+
+// runs once
+void setup() {
+    gpio_init(LED_PIN); // Use built-in LED to signal wake time
+    gpio_set_dir(LED_PIN, GPIO_OUT); // it is an output pin
+
+    // ss1306-OLED is initialized
+    oled_rc = myOled.init();
+    myOled.set_back_buffer(ucBuffer);
+    myOled.fill(0,1);
+
+    // Welcome screen
+    welcome(myOled);
+
+    // empty read as a warm-up
+    myBME280.measure();
+    sleep_ms(100);
+}
+
+// runs in each iteration
+void loop() { 
+    // get measurement from BME280
+    // start of measurement => LED HIGH
+    gpio_put(LED_PIN, 1);
+    // start actual measurement
+    result = myBME280.measure();
+    // end of measurement => LED LOW
+    gpio_put(LED_PIN, 0);
+    // write to OLED
+    draw_on_oled(myOled, result);
+}
+
+int main() {
+    stdio_init_all();
+
+    sleep_ms(3000); // required by some OSses to make Pico visible
+        
+    // Change frequency of Pico to a lower value
+    printf("Changing system clock to lower frequency: %d KHz\n", SYSTEM_FREQUENCY_KHZ);
+    set_sys_clock_khz(SYSTEM_FREQUENCY_KHZ, true);
+    
+    
+    // configure Sleep instance with Dormant mode
+    // Parameters
+    // - loop() and setup() functions are passed as labmdas
+    // - WAKEUP_PIN where GPIO signals are detected
+    // - in this case (true, false) means => detect 
+    //   leading edge with  WAKE_UP pin being active high
+    Sleep::instance().configure(setup, loop, WAKEUP_PIN, true, true);
+
+/*  // using Sleep mode instead
+    Sleep::instance().configure(setup, loop, start, end);
+*/
+
+/*  // using Normal mode instead
+    Sleep::instance().configure(setup, loop);
+*/
+
+    // show clock frequencies
+    Sleep::instance().measure_freqs();
+
+    // start event loop
+    Sleep::instance().run();
+
+    return 0;
+}
